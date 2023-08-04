7cccc817f61f19e6cbd2f09706ce74c0aee74b40
JPartida91 <111408839+JPartida91@users.noreply.github.com>
2023-08-04 04:40:14 +0000

    I change the variable name.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 first_name = input('What is your first name?\n')
 last_name = input('What is your last name?\n')
-birthday = input('What is your birthday month?\n')
-print(f'Hi, {first_name} {last_name}. Your birthday month is {birthday}.')
+birth_month = input('What is your birthday month?\n')
+print(f'Hi, {first_name} {last_name}. Your birthday month is {birth_month}.')
