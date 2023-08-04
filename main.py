825e28e66fc2e552f50f53a9edcd2297d4e5b065
JPartida91 <111408839+JPartida91@users.noreply.github.com>
2023-08-04 04:27:06 +0000

    Added a birthday month input.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 first_name = input('What is your first name?\n')
 last_name = input('What is your last name?\n')
-print(f'Hi, {first_name} {last_name}.')
+birthday = input('What is your birthday month?\n')
+print(f'Hi, {first_name} {last_name}. Your birthday month is {birthday}.')
